16c716fbe7435f12bea60f9047bcfbfbae2e9c0d
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-10-20 11:40:40 -0300

    agregue una nueva paleta de colores pero no lo utilice

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 // red for icons: #e85e56
 // white for background and fonts light: #f4e9dc
 // black for background and fonts dark: #494949
+// new pallette colors for haikus
+//
+// --salmon: #F28E75ff;
+// --english-violet: #474167ff;
+// --gunmetal: #2A323Fff;
+// --bright-pink-crayola: #F34868ff;
+//
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -10,6 +17,10 @@ module.exports = {
         primary: "#e85e56",
         light: "#f4e9dc",
         dark: "#494949",
+        salmon: "#F28E75ff",
+        violet: "#474167ff",
+        gunmetal: "#2A323Fff",
+        pink: "#F34868ff",
       },
     },
   },
