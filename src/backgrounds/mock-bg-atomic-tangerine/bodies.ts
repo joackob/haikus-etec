880201adde1c97d5b2ee51096037963b169ad28b
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-02-04 21:21:40 -0300

    refactor: ahora si usamos las facultades de Astro

--- src/backgrounds/mock-bg-atomic-tangerine/bodies.ts
+++ src/backgrounds/mock-bg-atomic-tangerine/bodies.ts
@@ -1,5 +1,11 @@
 import { Bodies } from "matter-js";
 
+import {
+  quinacridoneMagentaColor,
+  brightPinkCrayolaColor,
+  atomicTangerineColor,
+} from "./colors";
+
 interface ICircle {
   x: number;
   y: number;
@@ -31,3 +37,53 @@ export const rectangle = ({
     isStatic,
     render: { fillStyle: fillColor },
   });
+
+export const createBodies = (size: DOMRect) => {
+  const wight = 1;
+  const floor = rectangle({
+    x: size.width / 2,
+    y: size.height,
+    width: size.width,
+    height: wight,
+    fillColor: "transparent",
+    isStatic: true,
+  });
+  const leftWall = rectangle({
+    x: 0,
+    y: size.height / 2,
+    width: wight,
+    height: size.height,
+    fillColor: "transparent",
+    isStatic: true,
+  });
+  const rightWall = rectangle({
+    x: size.width,
+    y: size.height / 2,
+    width: wight,
+    height: size.height,
+    fillColor: "transparent",
+    isStatic: true,
+  });
+  const delta = 500;
+  const bar = rectangle({
+    x: size.width / 2,
+    y: 0 - delta,
+    width: size.width * 0.95,
+    height: size.height * 0.1,
+    fillColor: quinacridoneMagentaColor,
+  });
+  const disco = circle({
+    x: size.width / 4,
+    y: size.height / 2 - delta,
+    radio: size.width / 4,
+    fillColor: brightPinkCrayolaColor,
+  });
+  const column = rectangle({
+    x: size.width * 0.75,
+    y: size.height / 2 - delta,
+    width: size.width / 2,
+    height: size.height - size.height / 4,
+    fillColor: atomicTangerineColor,
+  });
+  return [disco, column, bar, rightWall, leftWall, floor];
+};
