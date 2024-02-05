f18145fe3a3c845df6253a41caa816acafd21e67
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-02-04 22:47:49 -0300

    refactor: deje de lado algunas interfaces y mejore la fluides de la caida

--- src/backgrounds/mock-bg-atomic-tangerine/bodies.ts
+++ src/backgrounds/mock-bg-atomic-tangerine/bodies.ts
@@ -38,51 +38,56 @@ export const rectangle = ({
     render: { fillStyle: fillColor },
   });
 
-export const createBodies = (size: DOMRect) => {
-  const wight = 1;
+interface PropsCreateBodies {
+  width: number;
+  height: number;
+}
+
+export const createBodies = (props: PropsCreateBodies) => {
+  const wight = 10;
   const floor = rectangle({
-    x: size.width / 2,
-    y: size.height,
-    width: size.width,
+    x: props.width / 2,
+    y: props.height,
+    width: props.width,
     height: wight,
     fillColor: "transparent",
     isStatic: true,
   });
   const leftWall = rectangle({
     x: 0,
-    y: size.height / 2,
+    y: props.height / 2,
     width: wight,
-    height: size.height,
+    height: props.height,
     fillColor: "transparent",
     isStatic: true,
   });
   const rightWall = rectangle({
-    x: size.width,
-    y: size.height / 2,
+    x: props.width,
+    y: props.height / 2,
     width: wight,
-    height: size.height,
+    height: props.height,
     fillColor: "transparent",
     isStatic: true,
   });
   const delta = 500;
   const bar = rectangle({
-    x: size.width / 2,
+    x: props.width / 2,
     y: 0 - delta,
-    width: size.width * 0.95,
-    height: size.height * 0.1,
+    width: props.width - wight,
+    height: props.height * 0.1,
     fillColor: quinacridoneMagentaColor,
   });
   const disco = circle({
-    x: size.width / 4,
-    y: size.height / 2 - delta,
-    radio: size.width / 4,
+    x: props.width / 4,
+    y: props.height / 2 - delta,
+    radio: props.width / 4 - wight,
     fillColor: brightPinkCrayolaColor,
   });
   const column = rectangle({
-    x: size.width * 0.75,
-    y: size.height / 2 - delta,
-    width: size.width / 2,
-    height: size.height - size.height / 4,
+    x: props.width * 0.75,
+    y: props.height / 2 - delta,
+    width: props.width / 2 - 2 * wight,
+    height: props.height - props.height / 4,
     fillColor: atomicTangerineColor,
   });
   return [disco, column, bar, rightWall, leftWall, floor];
